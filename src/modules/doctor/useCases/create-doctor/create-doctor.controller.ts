360f4866eea9d8429dcb4882646dc4eb482f0923
Philipe Faria <philipefaria61@gmail.com>
2022-12-05 18:32:34 -0300

    feat: Implements doctor registration route and controller

---
+++ src/modules/doctor/useCases/create-doctor/create-doctor.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+
+import { ISpecialtyRepository } from '../../../specialty/repositories/specialty.repository';
+import { IUserRepository } from '../../../users/repositories/user.repository';
+import { IDoctorRepository } from '../../repositories/doctor.repository';
+
+import { CreateDoctorUseCase } from './create-doctor.usecase';
+
+class CreateDoctorController {
+    constructor(
+        private userRepository: IUserRepository,
+        private doctorRepository: IDoctorRepository,
+        private specialtyRepository: ISpecialtyRepository,
+    ){}
+
+    async handle(request: Request, response: Response) {
+        const data = request.body;
+
+        const createDoctorUseCase = new CreateDoctorUseCase(
+            this.userRepository,
+            this.doctorRepository,
+            this.specialtyRepository
+        );
+
+        const result = await createDoctorUseCase.execute(data);
+
+        return response.json(data);
+    }
+}
+
+export { CreateDoctorController }
---
+++ src/modules/doctor/useCases/create-doctor/index.ts
@@ -0,0 +1,25 @@
+import { 
+    UserPrismaRepository
+} from '../../../users/repositories/implementations/user.prisma.repository';
+
+import {
+    DoctorPrismaRepository
+} from '../../repositories/implementations/doctor.prisma.repository';
+
+import {
+    SpecialtyRepository
+} from '../../../specialty/repositories/implementations/specialty.repository';
+
+import { CreateDoctorController } from './create-doctor.controller';
+
+const userRepository = new UserPrismaRepository();
+const doctorRepository = new DoctorPrismaRepository();
+const specialtyRepository = new SpecialtyRepository();
+
+const createDoctorController = new CreateDoctorController(
+    userRepository,
+    doctorRepository,
+    specialtyRepository
+);
+
+export { createDoctorController }
