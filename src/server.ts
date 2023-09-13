a4637d2ea61320016448f5b83b1bc80611a0c48f
Philipe Faria <philipefaria61@gmail.com>
2023-09-13 10:42:58 -0300

    test: Implement end-to-end testing

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,5 @@
 import 'dotenv/config';
-import express from 'express';
-import swaggerUI from 'swagger-ui-express';
 
-import swaggerDocument from '../swagger.json';
-import { router } from './routes';
-
-import './infra/cron/notification-appointment-day.cron';
-
-const app = express();
-
-app.use(express.json());
-
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
-app.use(router)
+import { app } from './app';
 
 app.listen(3333, () => console.log('Server is running on PORT 3333'));
